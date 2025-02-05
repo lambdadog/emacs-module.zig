60325143b1f152aca742cd11ded1e283995414d2
Ashlynn Anderson <github@pea.sh>
2025-02-05 06:41:09 +0000

    Improve error and remove now-redundant FAQ.md

--- src-entrypoint/root.zig
+++ src-entrypoint/root.zig
@@ -18,9 +18,14 @@ var gpl_token: c_int = undefined;
 comptime {
     if (options.gpl_compatible)
         @export(gpl_token, .{ .name = "plugin_is_GPL_compatible" })
-    else
-        @compileError(
-            // https://github.com/ziglang/zig/issues/22730
-            "Missing GPL affirmation. https://github.com/lambdadog/emacs-module.zig/blob/main/FAQ.md#missing-gpl-affirmation",
+    else // zig fmt: off
+        @compileError("Emacs refuses to load dynamic modules that don't specially signal that they are GPL-compatible.\n\r"
+          ++ "\n\r"
+          ++ "With emacs-module.zig, this is done by adding the following to the top level of whatever file is your root_source_file (usually, src/root.zig or src/main.zig, check build.zig if you aren't sure):\n\r"
+          ++ "\n\r"
+          ++ "  pub const module_options = .{\n\r"
+          ++ "    .gpl_compatible = true,\n\r"
+          ++ "  };\n\r"
         );
+    // zig fmt: on
 }
